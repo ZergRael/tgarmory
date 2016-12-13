bdddb002f94c6bc95348b4fc44ce232209d9575c
ZergRael <zergrael@gmail.com>
2016-12-13 21:45:38 +0100

    Typo in name fixer

--- tgarmory.js
+++ tgarmory.js
@@ -479,7 +479,7 @@ var ext = {
 
     fixName: function(name) {
       var $name = $('td[style="width:232px;text-align:center; color:#FFF; font-size:11px; padding-top:20px;"] strong');
-      if ($name.indexOf('?')) {
+      if ($name.text().indexOf('?')) {
         $name.text(name);
       }
     },
